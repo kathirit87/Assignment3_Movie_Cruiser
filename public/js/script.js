5e1661bb23dd671d803d8308509be964ecbc6829
kathirit87 <35557081+kathirit87@users.noreply.github.com>
2018-07-26 16:03:39 +0530

    automated test issue changes commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,46 +1,22 @@
 let moveList = [];
 let favMovieList = [];
 
-const postData = (url = ``, data = {}) => {
-	// Default options are marked with *
-	  return fetch(url, {
-		  method: "POST", // *GET, POST, PUT, DELETE, etc.
-		  mode: "cors", // no-cors, cors, *same-origin
-		  cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-		  credentials: "same-origin", // include, same-origin, *omit
-		  headers: {
-			  "Content-Type": "application/json; charset=utf-8",
-			  // "Content-Type": "application/x-www-form-urlencoded",
-		  },
-		  redirect: "follow", // manual, *follow, error
-		  referrer: "no-referrer", // no-referrer, *client
-		  body: JSON.stringify(data), // body data type must match "Content-Type" header
-	  })
-	  .then(response => response.json()) // parses response to JSON
-	  .catch(error => console.error(`Fetch Error =\n`, error));
-  };
-
 function getMovies() {
-	console.log("inside getmovies");
-
+	
     return fetch("http://localhost:3000/movies")
 	.then(response => response.json())
-	.then(movies => {
-		
+	.then(movies => {		
 		moveList = movies;
 		//document.getElementById('moviesList').innerHTML = moveList;
 		const moveListUL = document.getElementById('moviesList');
 		movies.forEach(element => {
-			
-			console.log('movieList ::'+element);
 			const li = document.createElement('li');
 			const liInnerHtml =`
 				<div class="list-group-item list-group-item-primary">
-					<h4 id="${element.id}" data-value=${element.title}>Movie : ${element.title}</h4>					
+					<h4 data-value=${element.title}>Movie : ${element.title}</h4>					
 					<img src="${element.posterPath}" class="mx-auto d-block"  alt="Poster image">					
 					<p class="d-flex textblock" >${element.overview} </p>
-					<button type="submit" class="btn btn-primary btn-block" onclick="addFavourite(${element.id})">AddToFavourites</button>
-					
+					<button type="submit" class="btn btn-primary btn-block" onclick="addFavourite(${element.id})">AddToFavourites</button>					
 				</div>
 			`;
 			li.innerHTML = liInnerHtml;
@@ -49,121 +25,92 @@ function getMovies() {
 		return movies;
 	})
 	.catch((err)=>{
-		return Promise.reject(Error(err))
+		return Promise.resolve(Error(err))
 	});
 	
 }
 
-function getFavourites() {
-
-	console.log("inside getFavourites");
-	const favListUL = document.getElementById('favouritesList');
-
+function getFavourites() {	
+	const favListUL = document.getElementById('favouritesList');	
     return fetch("http://localhost:3000/favourites")
 	.then(response => response.json())
 	.then(favourites => {
 		favMovieList = favourites;
-
-		favourites.forEach(element => {		
-			console.log('element.name ::'+element.title);			
+		favourites.forEach(element => {
 				const li = document.createElement('li');
+				li.id =element.id;
 				const liInnerHtml =`
 					<div class="list-group-item list-group-item-primary">
-						<h4 id="${element.id}" data-value=${element.title}>Movie : ${element.title}</h4>					
+						<h4 data-value=${element.title}>Movie : ${element.title}</h4>					
 						<img src="${element.posterPath}" class="mx-auto d-block"  alt="Poster image">					
-						<p class="d-flex textblock" >${element.overview} </p>
-						<button type="submit" class="btn btn-primary btn-block" onclick="removFavourite(${element.id})">RemoveFavourites</button>					
-					</div>
-				`;
-				li.innerHTML = liInnerHtml;
-				favListUL.appendChild(li);			
-				
-		});
-		return favourites;
-	})
-	.catch((err)=>{
-		return Promise.reject(Error(err))
-	});
-
-}
-
-function addFavourite(arg) {
-
-	console.log("inside addFavourite" +arg);
-
-	const favListUL = document.getElementById('favouritesList');
-
-	let isfavListExists = false;
-
-	// let movieNamebyId = document.getElementById("movieName");
-	// console.log('movieNamebyId ::'+movieNamebyId);
-	console.log("favMovieList : "+favMovieList);
-	if(favMovieList!== null){
-
-		favMovieList.forEach(data => {
-			if(data!== null && data.id === arg){
-				console.log(data);
-				alert("Movie already exists in Favourites!!!!")
-				isfavListExists = true;
-				return;
-			}
-		})
-	}
-
-	console.log("isfavListExists flag :: "+isfavListExists);
-	console.log("moveList :: "+moveList);
-	if(!isfavListExists){
-		moveList.forEach(function (data) {
-		
-			console.log('element.name ::'+data.title);
-			if(data.id === arg){
-				const li = document.createElement('li');
-				const liInnerHtml =`
-					<div class="list-group-item list-group-item-primary">
-						<h4 id="${data.id}" data-value=${data.title}>Movie : ${data.title}</h4>					
-						<img src="${data.posterPath}" class="mx-auto d-block"  alt="Poster image">					
-						<p class="d-flex textblock" >${data.overview} </p>
-						<button type="submit" class="btn btn-primary btn-block" onclick="removFavourite(${data.id})">RemoveFavourites</button>					
+						<p class="d-flex textblock" >${element.overview} </p>											
 					</div>
 				`;
 				li.innerHTML = liInnerHtml;
 				favListUL.appendChild(li);
-				
-				favMovieList.push(data);
-				fetch(`http://localhost:3000/favourites`,{
-					method: "POST",
-					body :JSON.stringify(data),
-					headers:{
-						'Content-Type': 'application/json'
-					  }
-				})
-				.then(resdata => console.log(resdata))// JSON from `response.json()` call
-				.catch(error => console.error(Promise.reject(Error(err))));
-			}		
 		});
-	}
-		
+		return favourites;
+	})
+	.catch((err)=>{
+		return Promise.resolve(Error(err))
+	});
+
 }
 
-function removFavourite(id){
+let getMovie = (id, arr) => arr.find(movie => movie.id === id);
+
+function addFavourite(arg) {
+	
+	if(getMovie(Number(arg),favMovieList)){
+		alert("Movie is already added to favourites")
+		return Promise.reject({message: 'Movie is already added to favourites'});
+	}
+	const favListUL = document.getElementById('favouritesList');
+	let data = getMovie(Number(arg),moveList);
+	const li = document.createElement('li');
+	li.id = data.id;
+	const liInnerHtml =`
+		<div class="list-group-item list-group-item-primary">
+			<h4 data-value=${data.title}>Movie : ${data.title}</h4>					
+			<img src="${data.posterPath}" class="mx-auto d-block"  alt="Poster image">					
+			<p class="d-flex textblock" >${data.overview} </p>						
+		</div>
+	`;
+	li.innerHTML = liInnerHtml;
+	favListUL.appendChild(li);
+	return fetch(`http://localhost:3000/favourites`,{
+		method: "POST",
+		body :JSON.stringify(data),
+		headers:{
+			'Content-Type': 'application/json'
+			}
+	})
+	.then(() => favMovieList.push(data) && favMovieList)// JSON from `response.json()` call
+	.catch(error => Promise.resolve(Error(error)));
+}
+
+/* function removFavourite(val){
 	console.log(favMovieList);
+	let item = document.getElementById(val);
 	favMovieList.forEach(data => {
-		console.log(data);
-		if(data.id===id){
-			const list = document.getElementById('favouritesList');
-
-			list.removeChild(list.childNodes[0]);
-
-			return fetch(`http://localhost:3000/favourites`, {
+		console.log(data);		
+		if(item && item.id===data.id){
+			item.parentNode.removeChild(item);
+			favMovieList.splice(data);
+			return fetch(`http://localhost:3000/favourites`+`/`+data.id, {
 				method: 'delete',
-				body : JSON.stringify(data)
+				body : JSON.stringify(data),
+				headers:{
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Methods': 'DELETE'
+				  }
 			  })
 			  .then(response => console.log(response.json()))
-			  .catch(error => console.error(Promise.reject(Error(err))));
+			  .catch(error => console.error(Promise.resolve(Error(err))));
 		}
 
 	});
-}
+} */
 // invoke getMovies function and manipulate DOM 
 
 module.exports = {
